8890c78c69dc62b69addaac465a305780bc77c3c
Jon Lauridsen <mail@jonlauridsen.com>
2017-12-17 14:01:59 +0000

    Rely on lxml library to parse+manage html content

--- test/test_page_scraper.py
+++ test/test_page_scraper.py
@@ -1,48 +1,58 @@
 from os import path
 
+from lxml.html import fromstring
 from mock import Mock, patch
 
 from webscraper.lib.page_scraper import (
-    _find_links, _find_occurrences_of_words, _get_page_content,
-    recursively_find_words,
+    _find_links, _find_occurrences_of_words, recursively_find_words,
 )
 
 
 @patch('webscraper.lib.page_scraper._get_page_content',
-       Mock(side_effect=['foo bar baz']))
+       Mock(side_effect=[fromstring('foo bar baz')]))
 def test_recursively_find_words_on_one_page():
     result = recursively_find_words('n/a', ['foo'])
     assert result == {'foo': 1}
 
 
 @patch('webscraper.lib.page_scraper._get_page_content',
-       Mock(side_effect=['<a href="url">link text</a>', 'foo']))
+       Mock(side_effect=[fromstring('<a href="url">link text</a>'),
+                         fromstring('foo')]))
 def test_recursively_find_words_on_2nd_page():
     result = recursively_find_words('n/a', ['foo'])
     assert result == {'foo': 1}
 
 
 @patch('webscraper.lib.page_scraper._get_page_content',
-       Mock(side_effect=['<a href="url">link text</a>', 'foo']))
+       Mock(side_effect=[fromstring('<a href="url">foo</a>'),
+                         fromstring('foo')]))
 def test_recursively_respects_recurse_limit():
     result = recursively_find_words('n/a', ['foo'], limit=0)
-    assert result == {'foo': 0}
+    assert result == {'foo': 1}
 
 
 @patch('webscraper.lib.page_scraper._get_page_content',
-       Mock(side_effect=['<a href="url2">link text</a>foo',
-                         '<a href="url">link text</a>foo']))
-def test_recursively_only_visits_page_once():
-    result = recursively_find_words('url', ['foo'])
+       Mock(side_effect=[fromstring('<a href="url">foo</a>'),
+                         fromstring('<a href="url">foo</a>'),
+                         fromstring('<a href="url">foo</a>')]))
+def test_recursively_visits_one_depth_at_limit_1():
+    result = recursively_find_words('n/a', ['foo'], limit=1)
     assert result == {'foo': 2}
 
 
-def test_get_page_content():
-    assert '<head>' in _get_page_content('https://google.com')
+@patch('webscraper.lib.page_scraper._get_page_content',
+       Mock(side_effect=[
+           fromstring('<a href="url">link text</a>foo'),
+           fromstring('foo')
+       ]))
+def test_recursively_only_visits_page_once():
+    result = recursively_find_words('url', ['foo'])
+    assert result == {'foo': 1}
 
 
 def test_find_occurrences_of_words_in_simple_text():
-    result = _find_occurrences_of_words('foo bar bar', ['foo', 'bar'])
+    result = _find_occurrences_of_words(fromstring('foo bar bar'),
+                                        ['foo', 'bar'])
     assert list(result) == [('foo', 1), ('bar', 2)]
 
 
@@ -56,13 +66,25 @@ def test_find_occurrences_of_words_against_example():
     assert list(result) == [('foo', 0)]
 
 
+def test_find_occurrences_of_words_against_example2():
+    fixture = _get_fixture('stats.research.icann.org/dns/tld_report/index.html')
+    result = _find_occurrences_of_words(fixture, ['foo'])
+    assert list(result) == [('foo', 5)]
+
+
+def test_find_links():
+    result = _find_links('/', fromstring('<a href="url">link text</a>'))
+    assert len(list(result)) == 1
+
+
 def test_find_links_against_google():
     result = _find_links('google.com', _get_fixture('google.com'))
-    assert len(list(result)) == 20
+    assert len(list(result)) == 24
 
 
 def _get_fixture(name):
     fixture_path = path.abspath(
         path.join(path.dirname(__file__), 'fixtures', name))
-    with open(fixture_path) as f:
-        return f.read()
+    with open(fixture_path, mode='rb') as f:
+        content = f.read()
+        return fromstring(content)
--- webscraper/lib/page_scraper.py
+++ webscraper/lib/page_scraper.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Optional
 from urllib.parse import urljoin
 
 import lxml.html
@@ -10,13 +10,12 @@ def recursively_find_words(url, words, limit=3, visited=[]):
     print("Visiting", url, "limit:", limit)
     html = _get_page_content(url)
     visited.append(url)
-    if not html:
+    if html is None:
         return word_occurrences
 
     for word, count in _find_occurrences_of_words(html, words):
         if word not in word_occurrences:
             word_occurrences[word] = 0
-        print("Found {} {} times on {}".format(word, count, url))
         word_occurrences[word] += count
 
     if limit > 0:
@@ -39,24 +38,22 @@ def _combine_word_occurrences(a: dict, b: dict) -> dict:
     return result
 
 
-def _get_page_content(url):
+def _get_page_content(url) -> Optional[lxml.html.HtmlElement]:
     try:
         res = get(url)
-        content = res.text
+        return lxml.html.fromstring(res.text)
     except Exception as ex:
         print("Giving up getting URL '{}': {}".format(url, ex))
-        content = ''
-    return content
+        return None
 
 
-def _find_occurrences_of_words(html: str, words: Iterable) -> Iterator:
-    dom = lxml.html.fromstring(html)
+def _find_occurrences_of_words(dom: lxml.html.HtmlElement,
+                               words: Iterable) -> Iterator:
     content = dom.text_content()
     occurrences = [content.count(word) for word in words]
     return zip(words, occurrences)
 
 
-def _find_links(root: str, content: str) -> Iterator:
-    dom = lxml.html.fromstring(content)
-    for link in dom.xpath('//a/@href'):
+def _find_links(root: str, dom: lxml.html.HtmlElement) -> Iterator:
+    for element, attribute, link, pos in dom.iterlinks():
         yield urljoin(root, link)
